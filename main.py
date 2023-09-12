3a5529193de489d389a8fd8786bef7f82ba9f0e6
romannjoroge <roman.njoroge@njuguna.com>
2023-09-12 20:48:49 +0300

    making a request that accepts a JSON body

--- main.py
+++ main.py
@@ -20,5 +20,16 @@ def queryStringFunction(item_name: str):
 def namedParameterFunction(age: Union[int, None], name: str):
     return f"{name} is {age} years old"
 
+# Defining the structure of the POST JSON Body
+class DetailsBody(BaseModel):
+    name: str
+    age: Union[PositiveInt, None] = 0
+    occupation: Union[str, None] = "Computer Scientist" # Defining a value thats nullable and has a default value
+
+# Defining a route that accepts a JSON body
+@app.post("/details")
+def detailsFunction(person: DetailsBody):
+    return f"The person created is called {person.name}, is {person.age} years old and is a {person.occupation}"
+
 
 
